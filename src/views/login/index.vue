b7f5e76c075b3ad5d24be001d9c53c97561e305a
hehong <632681127@qq.com>
2023-11-25 18:44:12 +0800

    feat: 替换登录页面的icon组件，调整样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,24 +7,18 @@
       <!-- username -->
       <el-form-item>
         <span class="svg-container">
-          <el-icon>
-            <avatar />
-          </el-icon>
+          <svg-icon icon="user" />
         </span>
         <el-input placeholder="username" name="username" type="text" />
       </el-form-item>
       <!-- password -->
       <el-form-item>
         <span class="svg-container">
-          <el-icon>
-            <avatar />
-          </el-icon>
+          <svg-icon icon="password" />
         </span>
         <el-input placeholder="password" name="password" />
         <span class="show-pwd">
-          <el-icon>
-            <avatar />
-          </el-icon>
+          <svg-icon icon="eye" />
         </span>
       </el-form-item>
 
@@ -36,9 +30,7 @@
   </div>
 </template>
 
-<script setup>
-import { Avatar } from '@element-plus/icons'
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
 $bg: #2d3a4b;
@@ -60,14 +52,14 @@ $cursor: #fff;
     margin: 0 auto;
     overflow: hidden;
 
-    ::v-deep .el-form-item {
+    :deep(.el-form-item) {
       border: 1px solid rgba(255, 255, 255, 0.1);
       background: rgba(0, 0, 0, 0.1);
       border-radius: 5px;
       color: #454545;
     }
 
-    ::v-deep .el-input {
+    :deep(.el-input) {
       display: inline-block;
       height: 47px;
       width: 85%;
@@ -116,7 +108,7 @@ $cursor: #fff;
       font-weight: bold;
     }
 
-    ::v-deep .lang-select {
+    :deep(.lang-select) {
       position: absolute;
       top: 4px;
       right: 0;
