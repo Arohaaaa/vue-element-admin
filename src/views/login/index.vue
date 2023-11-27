40d2d8d36e7be3c5629343348f04a34eff73524e
hehong <632681127@qq.com>
2023-11-27 11:37:36 +0800

    feat: 登录处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form" :model="loginForm" :rules="loginRules">
+    <el-form
+      class="login-form"
+      :model="loginForm"
+      :rules="loginRules"
+      ref="loginFormRef"
+    >
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
@@ -33,7 +38,12 @@
       </el-form-item>
 
       <!-- 登录按钮 -->
-      <el-button type="primary" style="width: 100%; margin-bottom: 30px">
+      <el-button
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click="handleLogin"
+        :loading="loading"
+      >
         登录</el-button
       >
     </el-form>
@@ -42,6 +52,7 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useStore } from 'vuex'
 import { validatePassword } from './rules'
 
 // 表单
@@ -76,6 +87,29 @@ const onChangePwdType = () => {
   } else {
     passwordType.value = 'password'
   }
+}
+
+// 处理登录
+const loading = ref(false)
+const store = useStore()
+const loginFormRef = ref(null)
+const handleLogin = () => {
+  // 1. 进行表单校验
+  loginFormRef.value.validate((valid) => {
+    if (!valid) return
+
+    // 2. 触发登录
+    loading.value = true
+    store
+      .dispatch('user/login', loginForm.value)
+      .then(() => {})
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        loading.value = false
+      })
+  })
 }
 </script>
 
